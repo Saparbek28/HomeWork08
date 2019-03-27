b53ca1f5b4b417c8d783c9f52b0f15fd836c1c07
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-03-28 00:39:04 +0600

    adding file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(3);
+            array[0] = 4;
+            array[1] = 134;
+            array[2] = 3456;
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
